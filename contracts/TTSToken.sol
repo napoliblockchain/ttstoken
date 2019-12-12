9c5ce60a77c3d6b67d00b5d6cbb7890dc9e04b86
Antonio Della Porta <antoniodellaporta.13@protonmail.com>
2019-12-12 22:09:37 +0100

    Adds author

--- contracts/TTSToken.sol
+++ contracts/TTSToken.sol
@@ -1,8 +1,10 @@
-pragma solidity ^0.5.0;
+pragma solidity ^0.5.0; 
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/ERC20Detailed.sol";
 import "@openzeppelin/contracts/token/ERC20/ERC20Mintable.sol";
+
+/// @author Antonio Della Porta
 
 contract TTSToken is ERC20, ERC20Detailed, ERC20Mintable {
 	uint private INITIAL_SUPPLY = 1e3;
